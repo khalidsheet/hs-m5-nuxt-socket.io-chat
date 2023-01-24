78dd5f729ad5af574a948d0583a93b8c104d7f16
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-01-24 14:17:41 +0300

    fix: disconnected user repeated message

--- modules/socket.ts
+++ modules/socket.ts
@@ -9,23 +9,20 @@ export default defineNuxtModule({
     nuxt.hook("listen", (server) => {
       const socket = new Server(server);
       socket.on("connection", (io) => {
-        console.log(io.id, "has connected");
         registerEvents(io);
 
         io.on("disconnect", () => {
-          console.log(io.id, "disconnected");
-
-          io.broadcast.emit("join", <PublicMessage>{
-            date: new Date(),
-            from: io.id,
-            message: `${getClient(io.id)} just left the chat.`,
-            type: "system",
-          });
+          if (getClient(io.id)) {
+            io.broadcast.emit("join", <PublicMessage>{
+              date: new Date(),
+              from: io.id,
+              message: `${getClient(io.id)} just left the chat.`,
+              type: "system",
+            });
+          }
 
           removeClient(io.id);
-
           const clients = [...getClients()];
-
           io.broadcast.emit("update-client-list", clients);
         });
       });
